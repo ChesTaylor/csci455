c5df4dd0a39a17a771b175e6ed2da2072fe0682b
chestaylor <cwt5049@rit.edu>
2016-04-12 14:31:24 -0400

    Set default url to user login.

--- pollsite/pollsite/urls.py
+++ pollsite/pollsite/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from elections import views
 
 urlpatterns = [
     url(r'^elections/', include('elections.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.user_login, name='login')
 ]
